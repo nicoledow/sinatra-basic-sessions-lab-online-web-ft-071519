b4c072765a5d8ca39c28235ab79255bcf8821715
Nicole Dow <nikki.dow71@gmail.com>
2019-08-29 17:38:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "123"
+  end
+  
+  get '/' do
+    @session = session
+    erb :index
+  end
+  
+  post '/checkout' do
+    #binding.pry
+    session[:item] = params["item"]
+    @session = session
+    erb :checkout_page
+  end
+  
+  
 end
